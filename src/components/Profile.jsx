0bf718aa2cd1a1995e6a1c3445c56945ed377dae
JuliaLuk <julia1lukasiewicz@gmail.com>
2024-01-12 11:50:53 -0600

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,16 +1,16 @@
 // import clsx from 'clsx';
 import css from '../components/Profile.module.css';
 
-import { HiUser } from 'react-icons/hi';
-const UserMenu = ({ name }) => {
-  return (
-    <div>
-      <p>
-        <HiUser /> {name}
-      </p>
-    </div>
-  );
-};
+// import { HiUser } from 'react-icons/hi';
+// const UserMenu = ({ name }) => {
+//   return (
+//     <div>
+//       <p>
+//         <HiUser /> {name}
+//       </p>
+//     </div>
+//   );
+// };
 
 export const Profile = ({ user: { username, tag, location, active, avatar, stats } }) => {
   return (
@@ -19,7 +19,7 @@ export const Profile = ({ user: { username, tag, location, active, avatar, stats
         <div className={css.description}>
           <img src={avatar} alt={username} className={css.avatar} />
           <p className={css.name}>Name: {username}</p>
-          <div>{UserMenu}</div>
+          {/* <div>{UserMenu}</div> */}
           <p className={css.tag}>Nick Name: {tag}</p>
           <p className={css.location}>Location: {location}</p>
           <p className={css.active}>Status: {active ? 'Active' : 'Retired'}</p>
