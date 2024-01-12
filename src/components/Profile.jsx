34bb722ef0c6928167699f8b4bd7dfcfd311d11b
JuliaLuk <julia1lukasiewicz@gmail.com>
2024-01-12 11:30:04 -0600

    css added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,29 +1,30 @@
 // import clsx from 'clsx';
+import css from '../components/Profile.module.css';
 
 export const Profile = ({ user: { username, tag, location, active, avatar, stats } }) => {
   return (
     <>
       <div className="profile">
-        <div className="description">
-          <img src={avatar} alt={username} className="avatar" />
-          <p className="name">Name: {username}</p>
-          <p className="tag">Nick Name: {tag}</p>
-          <p className="location">Location: {location}</p>
-          <p className="active">Status: {active ? 'Active' : 'Retired'}</p>
+        <div className={css.description}>
+          <img src={avatar} alt={username} className={scc.avatar} />
+          <p className={css.name}>Name: {username}</p>
+          <p className={css.tag}>Nick Name: {tag}</p>
+          <p className={css.location}>Location: {location}</p>
+          <p className={css.active}>Status: {active ? 'Active' : 'Retired'}</p>
         </div>
 
-        <ul className="stats">
+        <ul className={css.stats}>
           <li>
-            <span className="label">Followers: </span>
-            <span className="value">{stats.followers}</span>
+            <span className={css.label}>Followers: </span>
+            <span className={css.value}>{stats.followers}</span>
           </li>
           <li>
-            <span className="label">Views: </span>
-            <span className="value">{stats.views}</span>
+            <span className={css.label}>Views: </span>
+            <span className={css.value}>{stats.views}</span>
           </li>
           <li>
-            <span className="label">Likes: </span>
-            <span className="value">{stats.likes}</span>
+            <span className={css.label}>Likes: </span>
+            <span className={css.value}>{stats.likes}</span>
           </li>
         </ul>
       </div>
