e767015be6061ae61ddac4a1a65358c523cbc834
JuliaLuk <julia1lukasiewicz@gmail.com>
2024-01-12 11:47:41 -0600

    modern and icons added

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,13 +1,25 @@
 // import clsx from 'clsx';
 import css from '../components/Profile.module.css';
 
+import { HiUser } from 'react-icons/hi';
+const UserMenu = ({ name }) => {
+  return (
+    <div>
+      <p>
+        <HiUser /> {name}
+      </p>
+    </div>
+  );
+};
+
 export const Profile = ({ user: { username, tag, location, active, avatar, stats } }) => {
   return (
     <>
       <div className="profile">
         <div className={css.description}>
-          <img src={avatar} alt={username} className={scc.avatar} />
+          <img src={avatar} alt={username} className={css.avatar} />
           <p className={css.name}>Name: {username}</p>
+          <div>{UserMenu}</div>
           <p className={css.tag}>Nick Name: {tag}</p>
           <p className={css.location}>Location: {location}</p>
           <p className={css.active}>Status: {active ? 'Active' : 'Retired'}</p>
