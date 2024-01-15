234f6f90b06dee82ec5557e0cbb33138958fd5f3
JuliaLuk <julia1lukasiewicz@gmail.com>
2024-01-15 15:12:03 -0600

    styles added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
 import users from '../users.json';
 import { PageTitle } from './PageTitle/PageTitle';
-
 import { UsersList } from './UsersList/UsersList';
+import css from '../components/App.module.css';
 
 export const App = () => {
   return (
-    <div>
+    <div className={css.wrapper}>
       <PageTitle text="the best instagramerses" />
       <UsersList person={users} />
     </div>
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,11 +1,12 @@
 // import clsx from 'clsx';
 
 import css from '../Profile/Profile.module.css';
-import { BsApple, BsEnvelopePaperFill } from 'react-icons/bs';
+import { BsApple, BsEnvelopePaperFill, BsEmojiSunglassesFill } from 'react-icons/bs';
 import defaultAvatar from './default-avatar.png';
 
 export const Profile = ({ user: { username, tag, location, active, avatar, stats } }) => {
   const profileImage = avatar || defaultAvatar;
+  const profileName = username || 'we do not know yet it';
 
   // const statusClasses = clsx(css.status, {
   //   [css.isActive]: active,
@@ -18,30 +19,33 @@ export const Profile = ({ user: { username, tag, location, active, avatar, stats
       <div className={css.profile}>
         <div className={css.description}>
           <img src={profileImage} alt={username} className={css.avatar} />
-          <p className={css.name}>Name: {username}</p>
-          <BsApple className={css.icon} size="20" />
-          <p className={css.tag}>Nick Name: {tag}</p>
+          <p className={css.name}>
+            <BsApple className={css.icon} size="18" />
+            name: {profileName}
+          </p>
+
+          <p className={css.tag}>
+            <BsEmojiSunglassesFill className={css.icon} size="18" />
+            nick: {tag}
+          </p>
 
           <p className={css.location}>
             {' '}
-            <BsEnvelopePaperFill />
-            Location: {location}
+            <BsEnvelopePaperFill className={css.icon} size="18" />
+            location: {location}
           </p>
-          <p className={statusClasses}>Status: {active ? 'Active' : 'Retired'}</p>
+          <p className={statusClasses}>status: {active ? 'Active' : 'Retired'}</p>
         </div>
 
         <ul className={css.stats}>
           <li>
-            <span className={css.label}>Followers: </span>
-            <span className={css.value}>{stats.followers}</span>
+            <span className={css.label}>followers: {stats.followers}</span>
           </li>
           <li>
-            <span className={css.label}>Views: </span>
-            <span className={css.value}>{stats.views}</span>
+            <span className={css.label}>views: {stats.views}</span>
           </li>
           <li>
-            <span className={css.label}>Likes: </span>
-            <span className={css.value}>{stats.likes}</span>
+            <span className={css.label}>likes: {stats.likes}</span>
           </li>
         </ul>
       </div>
